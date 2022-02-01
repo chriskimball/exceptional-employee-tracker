ed19442fdda98e8c011c2d6a59555dc9c3ee4757
chriskimball <92878250+chriskimball@users.noreply.github.com>
2022-02-01 02:28:30 -0800

    Added async function to view all employees

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const db = require('./db/connection');
 
+// const logo = require('asciiart-logo');
+// const config = require('./package.json');
+// console.log(logo(config).render());
+
+
+
+async function viewAllEmployees() {
+    const employees = await db.query(`SELECT * FROM employee`)
+
+    console.table(employees)
+}
+
+viewAllEmployees();
