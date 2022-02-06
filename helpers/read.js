3b2a98bd9d547941dadf004e979307ad8e54f49b
chriskimball <92878250+chriskimball@users.noreply.github.com>
2022-02-05 17:41:20 -0800

    Added read.js modular functions

--- helpers/read.js
+++ helpers/read.js
@@ -1,7 +1,7 @@
 const db = require("../db/connection");
 
-async function selectAllEmployees () {    
-    return db.query(`SELECT e.id, 
+async function selectAllEmployees() {
+  return db.query(`SELECT e.id, 
     e.first_name, 
     e.last_name, 
     r.title, 
@@ -12,55 +12,83 @@ async function selectAllEmployees () {
     JOIN role r ON r.id = e.role_id
     JOIN department d ON d.id = r.department_id
     LEFT JOIN employee m ON m.id = e.manager_id
-    ORDER BY id ASC;`)
+    ORDER BY id ASC;`);
 }
-async function selectEmployeesByManager () {    
-
+async function selectEmployeesByManager(manager_id) {
+  return db.query(
+    `SELECT e.first_name,
+        e.last_name,
+        r.title,
+        r.salary
+        FROM employee e
+        JOIN role r ON r.id = e.role_id
+        WHERE manager_id = ?;`,
+    [manager_id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      console.table(result);
+    }
+  );
 }
-async function selectEmployeesByDepartment () {    
-
+async function selectEmployeesByDepartment(department_id) {
+  return db.query(
+    `SELECT e.first_name,
+    e.last_name,
+    r.title,
+    r.salary
+    FROM employee e
+    JOIN role r ON r.id = e.role_id
+    WHERE department_id = ?;`,
+    [department_id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      console.table(result);
+    }
+  );
 }
-async function selectAllRoles () {    
- return db.query(`SELECT r.id,
- r.title,
- d.name,
- r.salary
- FROM role r
- JOIN department d ON r.department_id = d.id
- ORDER BY id asc;
+async function selectAllRoles() {
+  return db.query(`SELECT r.id,
+    r.title,
+    d.name,
+    r.salary
+    FROM role r
+    JOIN department d ON r.department_id = d.id
+    ORDER BY id asc;
  `);
 }
-async function selectAllDepartments () {    
- return db.query(`SELECT * 
+async function selectAllDepartments() {
+  return db.query(`SELECT * 
  FROM department 
  ORDER BY name ASC;`);
 }
 
-async function selectDepartmentBudget (department_id) {    
-
-    return db.query(
-        `SELECT d.name,
+async function selectDepartmentBudget(department_id) {
+  return db.query(
+    `SELECT d.name,
         SUM(r.salary) AS total_budget
         FROM department d
         JOIN role r ON r.department_id = d.id
         WHERE r.department_id = ?
         GROUP BY d.name;`,
-        [department_id],
-        (err, result) => {
-          if (err) {
-            console.log(err);
-          }
-          console.table(result);
-        //   inquire();
-        });
+    [department_id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      console.table(result);
+    }
+  );
 }
-
 
 module.exports = {
-    selectAllEmployees,
-    selectEmployeesByManager,
-    selectEmployeesByDepartment,
-    selectAllRoles,
-    selectAllDepartments,
-    selectDepartmentBudget,
-}
+  selectAllEmployees,
+  selectEmployeesByManager,
+  selectEmployeesByDepartment,
+  selectAllRoles,
+  selectAllDepartments,
+  selectDepartmentBudget,
+};
